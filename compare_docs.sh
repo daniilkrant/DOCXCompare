5fa87f1ba38fc9d8c59f352232e0bf54af916726
Daniil.Krant <daniil.krant@globallogic.com>
2021-07-21 23:34:14 +0300

    Check if all packages are installed

--- compare_docs.sh
+++ compare_docs.sh
@@ -25,23 +25,34 @@ then
    exit
 fi
 
-while true; do
-    read -p "Do you wish to install pandoc (sudo apt install pandoc)? [Y/n] " yn
+REQUIRED_PKG="pandoc"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  while true; do
+    read -p "Do you wish to install $REQUIRED_PKG (sudo apt install $REQUIRED_PKG)? [Y/n] " yn
     case $yn in
-        [Yy]* ) sudo apt install pandoc; break;;
+        [Yy]* ) sudo apt install $REQUIRED_PKG; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+fi
 
-while true; do
-    read -p "Do you wish to install pandoc (sudo apt install meld)? [Y/n] " yn
+
+REQUIRED_PKG="meld"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  while true; do
+    read -p "Do you wish to install $REQUIRED_PKG (sudo apt install $REQUIRED_PKG)? [Y/n] " yn
     case $yn in
-        [Yy]* ) sudo apt install meld; break;;
+        [Yy]* ) sudo apt install $REQUIRED_PKG; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+fi
 
 echo "Converting $old_ver_docx_path and $new_ver_docx_path to .md"
 
@@ -55,3 +66,9 @@ echo "Converted $old_ver_docx_path to $old_ver_md_path"
 echo "Converted $new_ver_docx_path to $new_ver_md_path"
 
 meld $old_ver_md_path $new_ver_md_path
+
+rm $old_ver_md_path
+echo "Removed $old_ver_md_path"
+
+rm $new_ver_md_path
+echo "Removed $new_ver_md_path"
